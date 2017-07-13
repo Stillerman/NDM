51ad07c7f2eca36718369151c430bf6f27845dcd
Josh Stillerman <jas@psfc.mit.edu>
2017-07-13 16:00:21 -0400

    add inividual miniproposals and list of them

--- node/src/schema.js
+++ node/src/schema.js
@@ -16,6 +16,7 @@ import {
   GraphQLObjectType,
   GraphQLSchema,
   GraphQLString,
+  GraphQLInt
 } from 'graphql';
 import Viewer from './types/Viewer';
 import fetch from 'node-fetch';
@@ -44,6 +45,35 @@ const User = new GraphQLObjectType({
     }
   }
 });
+
+const MiniProposal = new GraphQLObjectType({
+  name: 'MiniProposal',
+  description: 'This is a miniproposal type',
+
+  fields() {
+    return {
+    mp: {type: GraphQLInt},
+    id: {type: GraphQLID},
+    title: {type: GraphQLString},
+    username: {type: GraphQLString},
+    url: {type: GraphQLString},
+    status: {type: GraphQLString},
+    file_date: {type: GraphQLString},
+    icrf_80_mhz: {type: GraphQLInt},
+    icrf_70_mhz: {type: GraphQLInt},
+    icrf_50_mhz: {type: GraphQLInt},
+    cryopump: {type: GraphQLInt},
+    dnb: {type: GraphQLInt},
+    rev_b: {type: GraphQLInt},
+    high_b: {type: GraphQLInt},
+    boronize_overnight: {type: GraphQLInt},
+    boronize_shots: {type: GraphQLInt},
+    comment: {type: GraphQLString},
+    date_filed: {type: GraphQLString},
+    }
+  }
+});
+
 
 export default new GraphQLSchema({
   query: new GraphQLObjectType({
@@ -74,7 +104,32 @@ export default new GraphQLSchema({
           })
           .then(json => json.results)
         }
-      }
+      },
+      miniproposal: {
+        type: MiniProposal,
+        args: {id: {type: GraphQLString}},
+        resolve: (root, args) =>
+        fetch(`${BASE_URL}/miniproposals/${args.id}/`, {method: 'GET', headers:{ Authorization: 'Basic dGVzdGluZzpsZXRzdHJ5dGhpcw=='}})
+        .then(res => res.json())
+        .then(json => {
+          console.log(json)
+          return json
+        })
+        .then(json => json)
+      },
+      miniproposals: {
+        type: new GraphQLList(MiniProposal),
+        resolve: (root, args) => {
+          return fetch(`${BASE_URL}/miniproposals/`, {method: 'GET', headers:{ Authorization: 'Basic dGVzdGluZzpsZXRzdHJ5dGhpcw=='}})
+          .then(res => res.json())
+          .then(json => {
+            console.log(json.results)
+            return json
+          })
+          .then(json => json.results)
+        }
+      },
+
     },
   }),
 });
