00b27bdd86c1abea4bb06a0c03bf73c0dff3b551
Jason Stillerman <jason.t.stillerman@gmail.com>
2017-07-13 13:02:26 -0400

    Added gulp and babel

---
+++ node/README.md
@@ -0,0 +1,4 @@
+## Usage
+`npm install`
+`gulp build`
+`gulp serve`
--- node/src/index.js
+++ node/src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import graphQLHTTP from 'express-graphql';
 
-import schema from './schmema';
+import schema from './schema';
 
 const app = express();
 
