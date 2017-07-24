fe279d35b7ef04e5c5412aa6f01a8d73c3c8cce3
Josh Stillerman <jas@psfc.mit.edu>
2017-07-24 15:41:09 -0400

    add django debug

--- docker/code/logbook/settings.py
+++ docker/code/logbook/settings.py
@@ -151,3 +151,26 @@ GRAPHENE = {
 }
 
 INTERNAL_IPS=('172.17.0.1', '172.17.0.2', '127.0.0.1')
+
+LOGGING = {
+    'version': 1,
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        }
+    }
+}
+
