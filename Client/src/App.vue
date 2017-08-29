83494ef260558921fa27e19ef9fbf6b779b17d1d
Jason Stillerman <jason.t.stillerman@gmail.com>
2017-08-29 14:41:08 -0400

    Progress on authentication

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,13 +1,45 @@
 <template>
   <div id="app">
+    <button
+      class="btn btn-primary btn-margin"
+      v-if="!authenticated"
+      @click="login()">
+        Log In
+    </button>
+
+    <button
+      class="btn btn-primary btn-margin"
+      v-if="authenticated"
+      @click="logout()">
+        Log Out
+    </button>
     <img class="resize" src="./assets/logo.png">
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import AuthService from './auth/AuthService'
+
+const auth = new AuthService()
+
+const { login, logout, authenticated, authNotifier } = auth
+
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    authNotifier.on('authChange', authState => {
+      this.authenticated = authState.authenticated
+    })
+    return {
+      auth,
+      authenticated
+    }
+  },
+  methods: {
+    login,
+    logout
+  }
 }
 </script>
 
--- Client/src/router/index.js
+++ Client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Logbook from '@/components/Logbook'
+import Callback from '@/components/Callback'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Logbook',
       component: Logbook
+    },
+    {
+      path: '/callback',
+      name: 'Callback',
+      component: Callback
     }
   ]
 })
