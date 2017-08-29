cfc3bfcb163de700218bbfda826b280f59e19a80
Jason Stillerman <jason.t.stillerman@gmail.com>
2017-08-29 14:42:50 -0400

    removed secret

--- Client/src/auth/auth0-variables.js
+++ Client/src/auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
-  clientId: 'oI_dVq7VeVvRKtf7vNf8At9zeF-Mwl7L',
+  clientId: 'PUT YOUR THINK HERE',
   domain: 'psfc.auth0.com',
   callbackUrl: 'http://localhost:8080/#/callback',
   apiUrl: 'API_IDENTIFIER'
