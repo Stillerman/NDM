5e2451f6baa62f6dbf65831e044f13824c26b8c9
Jason Stillerman <jason.t.stillerman@gmail.com>
2017-08-29 14:41:08 -0400

    Progress on authentication

---
+++ Client/src/auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  clientId: 'oI_dVq7VeVvRKtf7vNf8At9zeF-Mwl7L',
+  domain: 'psfc.auth0.com',
+  callbackUrl: 'http://localhost:8080/#/callback',
+  apiUrl: 'API_IDENTIFIER'
+}
