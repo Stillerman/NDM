988fc4953b5d24bd22b83f971e841a27d850f42b
Josh Stillerman <jas@psfc.mit.edu>
2017-09-13 16:41:40 -0400

    allow dummy auth header for testing

--- docker/code/logbook/settings.py
+++ docker/code/logbook/settings.py
@@ -59,6 +59,19 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'logbook.urls'
 CORS_ORIGIN_ALLOW_ALL = True
 
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'testAuth',
+)
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
