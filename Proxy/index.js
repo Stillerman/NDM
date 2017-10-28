66f2204c6c2a823c6ba710d3a3158bbfce4906b7
Josh Stillerman <jas@psfc.mit.edu>
2017-10-28 14:23:22 -0400

    start on the proxy server actually doing things

--- Proxy/index.js
+++ Proxy/index.js
@@ -1,22 +1,95 @@
-var http = require('http')
-var httpProxy = require('http-proxy')
-
-var proxy = httpProxy.createProxyServer({})
-
-
+var http = require('http');
+// Should this all be done with promises instead of synchronously 
+// Do I need to finialize the get before insert in the database
+//
+var httpProxy = require('http-proxy');
+var sqlite3 = require('sqlite3').verbose();
+var proxy = httpProxy.createProxyServer({});
+//
+// hook up a database optonally creating it
+// for now use sqlite - have to think about what to do with this
+//
+let db = new sqlite3.Database('./db/user.db', (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the users database.');
+    var query = 'CREATE TABLE IF NOT EXISTS users (sub varchar(32) PRIMARY KEY, username varchar(32) NOT NULL,passowrd varchar(32) NOT NULL, token    varchar(32), table_constraint);'
+    db.run(query);
+    console.log('table created');
+});
+//  Database holds users
+//     sub - returned by auth0
+//     username - nickname returned by auth0
+//     password - generated and used to create acount
+//     token - stored in database for now
+// figure out the username and password
+// if there is no authorization header then 
+//   reject this request
+// if the token is already associated then
+//   use its username, password (verify it )?
+// else if the user has a record (already has a db account) 
+//   associate its token 
+//   use its username and password
+// else
+//   make them a database account
+//   make them a record of username and password
+//   associate this token
+//   use this username and password
+//
 proxy.on('proxyReq', function(proxyReq, req, res, options) {
-  proxyReq.setHeader('X-Special-Proxy-Header', 'foobar')
+    if ('authorization' in req.headers) {
+        console.log('auth header');
+        console.log(req.headers.authorization);
+        console.log('now do the select');
+        db.get("SELECT username, password from users where token=?", [req.headers.authorization], function(err, row) {
+            if (err) {
+                console.log("no row for " + req.headers.authorization);
+                var request = require("request");
+                var options = {
+                    method: 'POST',
+                    url: "https://mit-psfc.auth0.com/userinfo",
+                    headers: {
+                        'content-type': 'application/json',
+                        'authorization': req.headers.authorization,
+                    }
+                }
+                request(options, function(error, response, body) {
+                    if (error) throw new Error(error);
+                    console.log(body);
+                    bd = JSON.parse(body);
+                    console.log(bd.sub);
+                    console.log(bd.nickname);
+
+                    db.get('select username, password from users where sub = ?', [bd.sub], function(err, row) {
+                        if (err) {
+                            username = bd.nickname;
+                            password = Math.random().toString(36).substring(2);
+                            // createAccount(username, password);
+                            db.run('insert into users (sub, username, password, token) values (?,?,?,?)', [bd.sub, username, password, req.headers.authorizaton]);
+                            console.log("have to create a db user and insert it in the table - is there a way to recurse ?");
+                        }
+                        username = row.username;
+                        password = row.password;
+                    });
+                });
+            } else {
+                username = row.username;
+                password = row.password;
+            }
+        });
+    } else {}
+    proxyReq.setHeader('X-Special-Proxy-Header', 'foobar');
 });
 
 var server = http.createServer(function(req, res) {
-  // You can define here your custom logic to handle the request
-  // and then proxy the request.
+    // You can define here your custom logic to handle the request
+    // and then proxy the request.
 
-
-  proxy.web(req, res, {
-    target: 'http://127.0.0.1:8080'
-  });
+    proxy.web(req, res, {
+        target: 'http://127.0.0.1:8080'
+    });
 });
 
-console.log("listening on port 5050")
+console.log("listening on port 5050");
 server.listen(5050);
