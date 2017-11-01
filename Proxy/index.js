51f5a9459eaeae77f9a735d2b30fbb23eb57c95a
Jason <jason.t.stillerman@gmail.com>
2017-11-01 13:28:10 -0400

    Promisied proxy request

--- Proxy/index.js
+++ Proxy/index.js
@@ -1,7 +1,10 @@
 var http = require('http');
-// Should this all be done with promises instead of synchronously 
+// Should this all be done with promises instead of synchronously
 // Do I need to finialize the get before insert in the database
 //
+
+var rp = require('request-promise')
+
 var httpProxy = require('http-proxy');
 var sqlite3 = require('sqlite3').verbose();
 var proxy = httpProxy.createProxyServer({});
@@ -11,13 +14,13 @@ var tokens = {};
 // for now use sqlite - have to think about what to do with this
 //
 let db = new sqlite3.Database('./db/user.db', (err) => {
-    if (err) {
-        console.error(err.message);
-    }
-    console.log('Connected to the users database.');
-    var query = 'CREATE TABLE IF NOT EXISTS users (sub varchar(32) PRIMARY KEY, username varchar(32) NOT NULL,password varchar(32) NOT NULL, table_constraint);'
-    db.run(query);
-    console.log('table created');
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the users database.');
+  var query = 'CREATE TABLE IF NOT EXISTS users (sub varchar(32) PRIMARY KEY, username varchar(32) NOT NULL,password varchar(32) NOT NULL, table_constraint);'
+  db.run(query);
+  console.log('table created');
 });
 //  Database holds users
 //     sub - returned by auth0
@@ -25,12 +28,12 @@ let db = new sqlite3.Database('./db/user.db', (err) => {
 //     password - generated and used to create acount
 //     token - stored in database for now
 // figure out the username and password
-// if there is no authorization header then 
+// if there is no authorization header then
 //   reject this request
 // if the token is already associated then
 //   use its username, password (verify it )?
-// else if the user has a record (already has a db account) 
-//   associate its token 
+// else if the user has a record (already has a db account)
+//   associate its token
 //   use its username and password
 // else
 //   make them a database account
@@ -39,78 +42,73 @@ let db = new sqlite3.Database('./db/user.db', (err) => {
 //   use this username and password
 //
 proxy.on('proxyReq', function(proxyReq, req, res, options) {
-    var username;
-    var password;
-    if (tok in tokens) {
-        username = tokens[tok].username;
-        password = tokens[tok].password;
-        proxyReq.setHeader('Authorization', 'Basic ' + new Buffer(username + ':' + password).toString('base64'));
-    }
+  var username;
+  var password;
+  if (tok in tokens) {
+    username = tokens[tok].username;
+    password = tokens[tok].password;
+    proxyReq.setHeader('Authorization', 'Basic ' + new Buffer(username + ':' + password).toString('base64'));
+  }
 });
 
 var server = http.createServer(function(req, res) {
-    // You can define here your custom logic to handle the request
-    // and then proxy the request.
+  // You can define here your custom logic to handle the request
+  // and then proxy the request.
 
-    var username;
-    var password;
-    if ('authorization' in req.headers) {
-        auth_header = req.headers;
-        tok = auth_header.authorization;
-        tok = tok.split(" ")[1];
-        if (!(tok in tokens)) {
-            console.log("token for " + req.headers.authorization + " not stored");
-            var request = require("request");
-            var options = {
-                method: 'POST',
-                url: "https://mit-psfc.auth0.com/userinfo",
-                headers: {
-                    'content-type': 'application/json',
-                    'authorization': req.headers.authorization,
-                }
-            }
-            request(options, function(error, response, body) {
-                if (error) {
-                    console.log('Authentication service returned an error '+ error);
-                    res.statusCode = 500
-                    res.end = 'Authentication service returned an error ' + error; 
-                }
-                else { 
-                    console.log(body);
-                    if (body.startsWith('Unauthorized')) {
-                        res.statusCode = 401
-                        res.end = 'Authentication service returned an error ' + error;
-                    }
-                    else {
-                        bd = JSON.parse(body);
-                        db.get('select username, password from users where sub = ?', [bd.sub], function(err, row) {
-                            if (err || !(row)) {
-                                console.log('row is not in database');
-                                username = bd.nickname;
-                                password = Math.random().toString(36).substring(2);
-                                // createAccount(username, password);
-                                tokens[tok] = {
-                                    'username': username,
-                                    'password': password
-                                };
-                                console.log('username: ' + username, '  password: '+ password);
-                                db.run("insert into users (sub, username, password) values(?, ?, ?)", [bd.sub, username, password], function(err){ console.log(err)});
-                                console.log("inserted");
-                            } else {
-                                tokens[tok] = {
-                                    'username': row.username,
-                                    'password': row.password
-                                };
-                            }
-                        })
-                    }
-                 }
-            });
+  var username;
+  var password;
+  if ('authorization' in req.headers) {
+    tok = req.headers.authorization.split(" ")[1]
+    if (!(tok in tokens)) {
+      console.log("token for " + req.headers.authorization + " not stored");
+      rp({
+        method: 'POST',
+        url: "https://mit-psfc.auth0.com/userinfo",
+        headers: {
+          'content-type': 'application/json',
+          'authorization': req.headers.authorization,
         }
-        proxy.web(req, res, {
-            target: 'http://127.0.0.1:8080'
-        });
+      })
+      .then((body) => {
+        console.log(body);
+        if (body.startsWith('Unauthorized')) {
+          res.statusCode = 401
+          res.end = 'Authentication service returned an error ' + error;
+        }
+        else {
+          bd = JSON.parse(body);
+          db.get('select username, password from users where sub = ?', [bd.sub], function(err, row) {
+            if (err || !(row)) {
+              console.log('row is not in database');
+              username = bd.nickname;
+              password = Math.random().toString(36).substring(2);
+              // createAccount(username, password);
+              tokens[tok] = {
+                'username': username,
+                'password': password
+              };
+              console.log('username: ' + username, '  password: '+ password);
+              db.run("insert into users (sub, username, password) values(?, ?, ?)", [bd.sub, username, password], function(err){ console.log(err)});
+              console.log("inserted");
+            } else {
+              tokens[tok] = {
+                'username': row.username,
+                'password': row.password
+              };
+            }
+          })
+        }
+      })
+      .catch(err => {
+        console.log('Authentication service returned an error '+ error);
+        res.statusCode = 500
+        res.end = 'Authentication service returned an error ' + error;
+      })
     }
+  }
+  proxy.web(req, res, {
+    target: 'http://127.0.0.1:8080'
+  });
 });
 
 console.log("listening on port 5050");
