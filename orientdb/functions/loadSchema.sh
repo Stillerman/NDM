cadfa7867c253e77d0612fdc2f13411ee9fdbae8
Josh Stillerman <jas@psfc.mit.edu>
2018-02-12 10:05:51 -0500

    An initial schema for HTS tape tests

---
+++ orientdb/functions/loadSchema.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+node AddClass.js ../../Schema/annotation.json 
+node AddClass.js ../../Schema/reel.json
+node AddClass.js ../../Schema/tape.json
+node AddClass.js ../../Schema/runDay.json
+node AddClass.js ../../Schema/shot.json
