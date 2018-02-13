5c3883d761e43291309221b15e4765c85e1137f9
Josh Stillerman <jas@psfc.mit.edu>
2018-02-13 13:32:25 -0500

    Add schema (#9)
    * parameterize database name and user passwords.  - almost works
    
    * updated to babel-env
    
    * add newline
    
    * dist is now in .gitignore
    
    * allow client-side javascript to see environment variable for dbname
    
    * Schema stuff
    
    * initial schema for reel and NDMobject
    
    * do the functions definitions first
    
    * getUser is actually GetUser
    
    * fix GetUser
    
    * An initial schema for HTS tape tests
    
    * remove note linked field.  The note will just be the first annoation
    (we are not sure what first means yet)
    of the object. Having it also as a linked field is problematic for 2 reasons
         1 - It is a denormalization of the records
         2 - there is a chicken and egg problem.
              - do you create the thing, or its anotation first.  They have to refer to eachother
    
    * update auth0
    
    * ujpdate vue2-editor to remove babel-preset complaint
    
    * include package-lock.json
    
    * babel-preset-env should only be prod, dev will get it from there
    
    * add externalReference class
    
    * Add external MDSplus reference class
    
    * just subclass the external not _ndmobject too, (external takes care of that)

---
+++ orientdb/functions/loadSchema.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+node AddClass.js ../../Schema/annotation.json 
+node AddClass.js ../../Schema/externalReference.json
+node AddClass.js ../../Schema/reel.json
+node AddClass.js ../../Schema/tape.json
+node AddClass.js ../../Schema/runDay.json
+node AddClass.js ../../Schema/shot.json
+node AddClass.js ../../Schema/MDSplus.json
