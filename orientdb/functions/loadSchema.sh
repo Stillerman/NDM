222b55558396538e0d67bbae46ffaeb0847e8992
Josh Stillerman <jas@psfc.mit.edu>
2018-02-13 10:03:15 -0500

    add externalReference class

--- orientdb/functions/loadSchema.sh
+++ orientdb/functions/loadSchema.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 node AddClass.js ../../Schema/annotation.json 
+node AddClass.js ../../Schema/externalReference.json
 node AddClass.js ../../Schema/reel.json
 node AddClass.js ../../Schema/tape.json
 node AddClass.js ../../Schema/runDay.json
